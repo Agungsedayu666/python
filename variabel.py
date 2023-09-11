9b246887d35f51251112bdf2dd225556b6387c14
DESKTOP-87AG12V\prioh <prio.ho.agung@gmail.com>
2023-09-11 14:45:05 +0700

    variabel

---
+++ variabel.py
@@ -0,0 +1,21 @@
+a = 10
+x = 5
+panjang = 1000
+
+# pemanggilan ke-1
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+nilai_y = 15
+juta10 = 10000000
+nilaiZ = 17.5
+
+# pemanggilan ke-2
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assigmnment indirect
+b = a
+print("Nilai b = ", b)
