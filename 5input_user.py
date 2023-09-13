20d91903902588db0d424be8f21b6c870f5209b7
Agungsedayu666 <67897210+Agungsedayu666@users.noreply.github.com>
2023-09-13 23:23:41 +0700

    mengambil data dari user (input user)

---
+++ 5input_user.py
@@ -0,0 +1,16 @@
+# mengambil data dari user (input user)
+
+# data yang dimasukan pasti string
+data = input("Masukan nama: ")
+
+print("data = ",data,",type =",type(data))
+
+# jika ingin mengambil data int, maka :
+data_int = int(input("masukan umur: "))
+
+print("data = ",data_int,",type =",type(data_int))
+
+# untuk mengabil data boolean
+biner = bool(int(input("masukan nilai boolean: ")))
+
+print("data = ",biner,",type =",type(biner))
